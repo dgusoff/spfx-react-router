7bcf6cd3c3596fc9e01cb154586197cd26ed6ed3
Derek Gusoff <dgusoff@rightpoint.com>
2019-01-23 11:44:51 -0500

    add sub components

--- src/webparts/spfxSpa/components/main-nav.tsx
+++ src/webparts/spfxSpa/components/main-nav.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import styles from './SpfxSpa.module.scss';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import Page1 from './page1';
+import Page2 from './page2';
+import Page3 from './page3';
+import Page4 from './page4';
+
 export default class MainNav extends React.Component {
     public render(): React.ReactElement<null> {
         return (<div>
@@ -23,9 +28,15 @@ export default class MainNav extends React.Component {
                             <Link to={`/closed/`}>CLOSED ISSUES</Link>
                         </li>
                     </ul>
+
+                    <div className="padContent"></div>
+                    <Route exact path={`/`} component={Page1} />
+                    <Route path={`/my/`} component={Page2} />
+                    <Route path={`/new`}  component={Page3} />
+                    <Route path={`/closed/`} component={Page4} />
                 </div>
-            </Router>
-        </div>
+            </Router >
+        </div >
         );
     }
 }
