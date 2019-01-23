b443477d52d3220ba0bbd791eef184b8d4331355
Derek Gusoff <dgusoff@rightpoint.com>
2019-01-23 11:19:03 -0500

    add css

--- README.md
+++ README.md
@@ -7,6 +7,22 @@ This is where you include your WebPart documentation.
 1. `yo sharepoint` a react web part
 2. add the components
 3. `npm install react-router react-router-dom`
+4. add router imports to nav component
+5. add router and links elements
+6. demo with browser router
+7. add imports for secondary components
+8. add route elements into router
+9. demo route switching
+10. demo problems - refresh
+11. demo adding page url
+13. talk about browserrouter, hashrouter and memoryrouter
+14. implement hashrouter and demo
+15. implement memoryrouter and demo
+16. add office fabric components
+17. package and dpeloy and demo on live site.
+
+
+
 
 
 
--- src/webparts/spfxSpa/components/main-nav.tsx
+++ src/webparts/spfxSpa/components/main-nav.tsx
@@ -1,32 +1,31 @@
 import * as React from 'react';
-//import styles from './SpfxSpa.modules.css';
+import styles from './SpfxSpa.module.scss';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 export default class MainNav extends React.Component {
     public render(): React.ReactElement<null> {
-        return (  <div>
+        return (<div>
             <p>Navigation</p>
-    
+
             <Router>
                 <div>
-            <ul className="navigation">
-              <li className="ms-fontWeight-semibold ms-fontSize-mPlus">
-                <Link to={`/`}>OPEN ISSUES</Link>
-              </li>
-              <li className="ms-fontWeight-semibold ms-fontSize-mPlus navLink">
-                <Link to={`/my/`}>MY ISSUES</Link>
-              </li>
-              <li className="ms-fontWeight-semibold ms-fontSize-mPlus navLink">
-                <Link to={`/new/`}>CREATE A NEW ISSUE</Link>
-              </li>
-              <li className="ms-fontWeight-semibold ms-fontSize-mPlus navLink">
-                <Link to={`/closed/`}>CLOSED ISSUES</Link>
-              </li>                    
-            </ul>
-          </div>
-                  
-            </Router>       
-            
-            </div>);
+                    <ul className={styles.navigation}>
+                        <li className="ms-fontWeight-semibold ms-fontSize-mPlus">
+                            <Link to={`/`}>OPEN ISSUES</Link>
+                        </li>
+                        <li className="ms-fontWeight-semibold ms-fontSize-mPlus navLink">
+                            <Link to={`/my/`}>MY ISSUES</Link>
+                        </li>
+                        <li className="ms-fontWeight-semibold ms-fontSize-mPlus navLink">
+                            <Link to={`/new/`}>CREATE A NEW ISSUE</Link>
+                        </li>
+                        <li className="ms-fontWeight-semibold ms-fontSize-mPlus navLink">
+                            <Link to={`/closed/`}>CLOSED ISSUES</Link>
+                        </li>
+                    </ul>
+                </div>
+            </Router>
+        </div>
+        );
     }
 }
